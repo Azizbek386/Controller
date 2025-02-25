aba6aa45c4048fe5d47d68ff83189c632a32f5e6
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-02-25 17:01:34 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 1. Barcha mahsulotlarni ko'rsatish
+Route::get('/products', [ProductController::class, 'index']);
+
+// 2. Yangi mahsulot qo'shish
+Route::get('/products/create', [ProductController::class, 'create']);
+
+// 3. Ma'lumotni ko'rsatish
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+// 4. Ma'lumotni tahrirlash
+Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
+
+// 5. Ma'lumotni o‘chirish
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+// 6. Resursli controller
+Route::resource('products', ProductController::class)->only(['index', 'create', 'show', 'edit', 'destroy']);
+
+// 7. Custom qidiruv
+Route::get('/products/search', [ProductController::class, 'search']);
