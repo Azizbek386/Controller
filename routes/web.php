0f178bd771acbe870e86f49821d8c5c0d972cd45
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-02-26 22:01:37 +0500

    this is routes post

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
 
 // 1. Barcha mahsulotlarni ko'rsatish
+
+
 Route::get('/products', [ProductController::class, 'index']);
 
 // 2. Yangi mahsulot qo'shish
 Route::get('/products/create', [ProductController::class, 'create']);
 
 // 3. Ma'lumotni ko'rsatish
+
 Route::get('/products/{id}', [ProductController::class, 'show']);
 
 // 4. Ma'lumotni tahrirlash
